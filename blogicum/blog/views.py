d45a6b727c86fe1db3f342064ead92c0dae538c5
Ahmed <ahmedzulkarnaev9500@gmail.com>
2023-11-04 19:06:56 +0300

    fix errors

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,27 +1,28 @@
-from django.db.models.functions import Now
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse, reverse_lazy
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
-from django.core.paginator import Paginator
+from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models.functions import Now
-from django.urls import reverse_lazy, reverse
-from django.contrib.auth.decorators import login_required
-from .forms import UserCreationForm
-from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
-from django.db.models import Count
+from django.contrib.auth import login
+from django.db.models import Count, Q
+from django.db.models.functions import Now
 
 from .models import Category, Post, User, Comment
 from .constants import POSTS_LIMIT
-from .forms import PostForm, CommentForm, UserProfileEditForm
+from .forms import PostForm, CommentForm, UserProfileEditForm, UserCreationForm
 
 
+# Функция для аннотации количества комментариев к постам
 def count_comment(queryset):
     return queryset.annotate(comment_count=Count('comments'))
 
 
+# Функция для получения опубликованных постов
 def get_published_posts(manager=Post.objects):
     return manager.filter(
         pub_date__lte=Now(),
@@ -30,6 +31,7 @@ def get_published_posts(manager=Post.objects):
     )
 
 
+# Класс для отображения списка постов
 class PostListView(ListView):
     model = Post
     template_name = 'blog/index.html'
@@ -43,6 +45,7 @@ class PostListView(ListView):
         return queryset
 
 
+# Класс для отображения деталей поста
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detail.html'
@@ -62,6 +65,7 @@ class PostDetailView(DetailView):
         return context
 
 
+# Миксин для проверки доступа при редактировании и удалении поста
 class DispatchMixin:
     def dispatch(self, request, *args, **kwargs):
         self.post_id = kwargs['pk']
@@ -70,18 +74,21 @@ class DispatchMixin:
         return super().dispatch(request, *args, **kwargs)
 
 
+# Класс для обновления поста
 class PostUpdateView(LoginRequiredMixin, DispatchMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/create.html'
 
 
+# Класс для удаления поста
 class PostDeleteView(LoginRequiredMixin, DispatchMixin, DeleteView):
     model = Post
     success_url = reverse_lazy('blog:index')
     template_name = 'blog/create.html'
 
 
+# Класс для создания поста
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
@@ -89,7 +96,14 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        return super().form_valid(form)
+        delayed_date = form.cleaned_data.get('pub_date')
+
+        if delayed_date and delayed_date > timezone.now():
+            form.instance.is_published = False
+            return super().form_valid(form)
+        else:
+            form.instance.is_published = True
+            return super().form_valid(form)
 
     def get_success_url(self):
         return reverse(
@@ -100,15 +114,22 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         )
 
 
+# Функция для отображения постов в категории
 def category_posts(request, category_slug):
     template_name = 'blog/category.html'
     category = get_object_or_404(
         Category,
-        slug=category_slug,
-        is_published=True
+        slug=category_slug
     )
-    posts = get_published_posts().filter(category=category)
-    page_obj = count_comment(posts)
+
+    posts = get_published_posts().filter(
+        Q(category=category, author=request.user) | Q(category=category)
+    ).order_by('-pub_date')
+
+    paginator = Paginator(posts, POSTS_LIMIT)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
         'category': category,
         'page_obj': page_obj,
@@ -117,6 +138,7 @@ def category_posts(request, category_slug):
     return render(request, template_name, context)
 
 
+# Функция для добавления комментария
 @login_required
 def add_comment(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -129,6 +151,7 @@ def add_comment(request, pk):
     return redirect('blog:post_detail', post_id=pk)
 
 
+# Функция для редактирования комментария
 @login_required
 def edit_comment(request, comment_id):
     comment = get_object_or_404(Comment, pk=comment_id)
@@ -144,6 +167,7 @@ def edit_comment(request, comment_id):
     return render(request, 'blog/create.html', {'form': form})
 
 
+# Функция для удаления комментария
 @login_required
 def delete_comment(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id)
@@ -156,6 +180,7 @@ def delete_comment(request, comment_id):
     return render(request, 'blog/create.html', {'form': form})
 
 
+# Класс для регистрации пользователей
 class UserRegistrationView(CreateView):
     template_name = 'registration/registration_form.html'
     form_class = UserCreationForm
@@ -168,22 +193,28 @@ class UserRegistrationView(CreateView):
         return response
 
 
+# Класс для входа пользователей
 class LoginView(LoginView):
     def get_success_url(self):
         username = self.request.user.get_username()
         return reverse('blog:profile', args=[username])
 
 
+# Функция для отображения профиля пользователя
+@login_required
 def user_profile(request, username):
     user = get_object_or_404(User, username=username)
-    posts = user.posts.select_related(
-        'category',
-        'author',
-        'location'
-    ).filter(
-        pub_date__lt=Now(),
-        category__is_published=True
-    )
+    posts = user.posts.select_related('category', 'author', 'location')
+    if user == request.user or request.user.is_staff:
+        posts = posts.filter(
+            Q(category__is_published=True) | Q(category__is_published=False)
+        )
+    else:
+        posts = posts.filter(
+            pub_date__lt=timezone.now(),
+            category__is_published=True
+        )
+
     posts = count_comment(posts).order_by('-pub_date')
     paginator = Paginator(posts, POSTS_LIMIT)
     page_number = request.GET.get('page')
@@ -196,6 +227,7 @@ def user_profile(request, username):
     return render(request, 'blog/profile.html', context)
 
 
+# Функция для редактирования профиля пользователя
 @login_required
 def edit_profile(request, username):
     user = get_object_or_404(User, username=username)
